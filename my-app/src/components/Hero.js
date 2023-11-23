1f44123d8e8bd6bdc3915b5ac54dead1d332c45a
RH-Kevin <147596439+RH-Kevin@users.noreply.github.com>
2023-11-24 07:13:12 +0900

    *Final* updates.

--- my-app/src/components/Hero.js
+++ my-app/src/components/Hero.js
@@ -1,10 +1,10 @@
 import React from "react";
-import {Box, Flex, SimpleGrid, Image, ChakraProvider } from "@chakra-ui/react";
+import {Box, Flex, SimpleGrid, Image, ChakraProvider, Heading } from "@chakra-ui/react";
 
 const Hero = () => {
   return (
     <ChakraProvider>
-    <Box backgroundColor="#495E57">
+    <Box backgroundColor="#495E57" p={10}>
       <div>
         <Flex
           margin="0 auto"
@@ -13,18 +13,19 @@ const Hero = () => {
           justifyContent="center"
           alignItems="center"
           maxWidth="1024px"
-          height={300}
+          height={260}
+          
         >
           <SimpleGrid columns={3} spacing={10} id="heroText">
-            <Box bg='#495E57' height='50px'><h1>Little Lemon</h1></Box>
-            <Box bg='#495E57' height='50px'></Box>
-            <Box bg='#495E57' height='50px' display="grid" placeItems="center"><Image src="./icons_assets/2/resturantchefB.png" alt="chef" width="50px"></Image></Box>
-            <Box bg='#495E57' height='50px'><h3>Chicago</h3></Box>
-            <Box bg='#495E57' height='50px'></Box>
-            <Box bg='#495E57' height='50px'></Box>
-            <Box bg='#F4CE14' height='50px' borderRadius={10} color="black" display="grid" placeItems="center"><a className="button" href="">Book a Table</a></Box>
-            <Box bg='#F4CE14' height='50px' borderRadius={10} color="black" display="grid" placeItems="center"><a className="button" href="">Order Online</a></Box>
-            <Box bg='#495E57' height='50px'></Box>
+            <Box bg='#495E57' height='40px'><Heading id="title" as='h1' size="2xl">Little Lemon</Heading></Box>
+            <Box bg='#495E57' height='40px'></Box>
+            <Box bg='#495E57' height='40px' display="grid" placeItems="center"><Image src="./icons_assets/2/resturantchefB.png" alt="chef" width="50px"></Image></Box>
+            <Box bg='#495E57' height='40px'><Heading id="branch" as='h3' size="lg">Chicago</Heading></Box>
+            <Box bg='#495E57' height='40px'></Box>
+            <Box bg='#495E57' height='40px'></Box>
+            <Box bg='#F4CE14' height='40px' borderRadius={10} color="black" display="grid" placeItems="center"><a className="button" href="">Book a Table</a></Box>
+            <Box bg='#F4CE14' height='40px' borderRadius={10} color="black" display="grid" placeItems="center"><a className="button" href="">Order Online</a></Box>
+            <Box bg='#495E57' height='40px'></Box>
           </SimpleGrid>
         </Flex>
       </div>
--- my-app/src/components/ReservationForm.js
+++ my-app/src/components/ReservationForm.js
@@ -37,28 +37,42 @@ const getCurrentDate = () => {
 
 const ReservationForm = () => {
 
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        date: '',
+        time: '',
+        partySize: '',
+        type: '',
+        notes: '',
+      });
+
+
     const formik = useFormik({
-        initialValues: {
-            firstName: '',
-            lastName: '',
-            email: '',
-            phoneNumber: '',
-            date: '',
-            time: '',
-            partySize: '',
-            type: '',
-            notes: '',
-        },
-        validationSchema: Yup.object({ 
-            firstName: Yup.string().required("Required"), lastName: Yup.string().required("Required"),
-            email: Yup.string().email("Invalid email address").required("Required"),
-            phoneNumber: Yup.string().required("Required"), date: Yup.date().required("Required"),
-            time: Yup.string().required("Requred"), partySize: Yup.string().required("Required"),
-            notes: Yup.string().min(25, "Must be at least 25 characters"),
-          }),
+
+            initialValues: formData,
+            validationSchema: Yup.object({ 
+                firstName: Yup.string().required("Required"), lastName: Yup.string().required("Required"),
+                email: Yup.string().email("Invalid email address").required("Required"),
+                phoneNumber: Yup.string().required("Required"), date: Yup.date().required("Required"),
+                time: Yup.string().required("Requred"), partySize: Yup.string().required("Required"),
+                notes: Yup.string().min(25, "Must be at least 25 characters"),
+              }),
+            onSubmit: (values) => {
+                console.log("Form submitted:", values);
+            },
     });
 
-    const occasionOptions = ['Wedding', 'Graduation', 'Other'];
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+        formik.handleChange(e); // Call formik's handleChange to handle validation
+      };
 
     return (
         <ChakraProvider>
@@ -83,28 +97,24 @@ const ReservationForm = () => {
             <VStack width={{ base: "100%", md: "48%" }} spacing={4}>
                 <FormControl isInvalid={!!formik.errors.firstName && formik.touched.firstName}>
                 <FormLabel htmlFor="firstName">First Name</FormLabel>
-                <Input
-                    id="firstName"
-                    name="firstName"
-                    {...formik.getFieldProps("firstName")}
-                />
+                <Input id="firstName" name="firstName" value={formData.firstName} onChange={handleInputChange}/>
                 <FormErrorMessage>{formik.errors.firstName}</FormErrorMessage>
                 </FormControl>
                 <FormControl isInvalid={!!formik.errors.lastName && formik.touched.lastName}>
                 <FormLabel htmlFor="lastName">Last Name</FormLabel>
-                <Input id="lastName" name="lastName" {...formik.getFieldProps("lastName")}/>
+                <Input id="lastName" name="lastName" value={formData.lastName} onChange={handleInputChange}/>
                 <FormErrorMessage>{formik.errors.lastName}</FormErrorMessage>
                 </FormControl>
 
                 <FormControl isInvalid={!!formik.errors.email && formik.touched.email}>
                     <FormLabel htmlFor="email">Email Address</FormLabel>
-                    <Input id="email" name="email" type="email" {...formik.getFieldProps("email")} />
+                    <Input id="email" name="email" type="email" value={formData.email} onChange={handleInputChange} />
                     <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
                 </FormControl>
 
                 <FormControl isInvalid={!!formik.errors.phoneNumber && formik.touched.phoneNumber}>
                 <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
-                <Input id="phoneNumber" name="phoneNumber" {...formik.getFieldProps("phoneNumber")}/>
+                <Input id="phoneNumber" name="phoneNumber" value={formData.email} onChange={handleInputChange}/>
                 <FormErrorMessage>{formik.errors.phoneNumber}</FormErrorMessage>
                 </FormControl>
             </VStack>
@@ -113,13 +123,13 @@ const ReservationForm = () => {
 
                 <FormControl isInvalid={!!formik.errors.date && formik.touched.date}>
                 <FormLabel htmlFor="date">Date</FormLabel>
-                <Input id="date" type="date" name="date" onChange={formik.handleChange} onBlur={formik.handleBlur} value={formik.values.date} min={getCurrentDate()}/>
+                <Input id="date" type="date" name="date" onChange={handleInputChange} onBlur={formik.handleBlur} value={formik.values.date} min={getCurrentDate()}/>
                 <FormErrorMessage>{formik.touched.date && formik.errors.date}</FormErrorMessage>
                 </FormControl>
 
                 <FormControl>
                 <FormLabel htmlFor="type">Time</FormLabel>
-                <Select id="time" name="time">
+                <Select id="time" name="time" value={formData.time} onChange={handleInputChange}>
                  <option value="default">Select a Time</option>
                  <option value="17:00">17:00</option>
                  <option value="18:00">18:00</option>
@@ -132,12 +142,12 @@ const ReservationForm = () => {
 
                 <FormControl isInvalid={!!formik.errors.partySize && formik.touched.partySize}>
                 <FormLabel htmlFor="partySize">Party Size</FormLabel>
-                <Input type="number" placeholder='1' id="partySize" name="partySize" min="1" max="10"/>
+                <Input type="number" placeholder='1' id="partySize" name="partySize" min="1" max="10" value={formData.partySize} onChange={handleInputChange}/>
                 <FormErrorMessage>{formik.touched.partySize && formik.errors.partySize}</FormErrorMessage>
                 </FormControl>
                 <FormControl>
                 <FormLabel htmlFor="type">Occasion</FormLabel>
-                <Select id="type" name="type" {...formik.getFieldProps("type")}>
+                <Select id="type" name="type" value={formData.type} onChange={handleInputChange}>
                  <option value="default">Select Occasion</option>
                  <option value="wedding">Wedding</option>
                  <option value="anneversary">Anneversary</option>
@@ -153,17 +163,17 @@ const ReservationForm = () => {
 
                <HStack p={0}>
                <Box mt={0} width="100%">
-                
             <FormLabel htmlFor="notes">Notes</FormLabel>
                 <Textarea
                  id="notes"
                  name="notes"
                  height={50}
+                 value={formData.notes} onChange={handleInputChange}
                />
             </Box>
 
             <Box mt={8}>
-            <Button type="submit" colorScheme="purple" width="full">Submit</Button>
+            <Button type="submit" colorScheme="purple" width="full" onClick={formik.handleSubmit}>Submit</Button>
             </Box>
              </HStack>
              <HStack p={0}></HStack>
